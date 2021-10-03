971f64d96a21b0a9cdb1b4d11b82a455554c34d3
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2021-10-03 18:56:45 -0500

    Update 2021_09_13_235236_create_posts_view.php

--- database/migrations/2021_09_13_235236_create_posts_view.php
+++ database/migrations/2021_09_13_235236_create_posts_view.php
@@ -9,7 +9,7 @@ class CreatePostsView extends Migration
 {
     public function up(): void
     {
-        DB::statement(file_get_contents(database_path('views\followedPosts.sql')));
+        DB::statement(file_get_contents(database_path('views/followedPosts.sql')));
     }
 
     public function down(): void
